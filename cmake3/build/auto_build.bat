d810871dca762d8b1be0680cb6c384d5d32bd7fe
Michael Pan <panyingyun@gmail.com>
2018-12-03 11:09:15 +0800

    修改 window下的脚本，传统的 include_directories link_directories 包含库和包含头文件的方法

--- cmake3/build/auto_build.bat
+++ cmake3/build/auto_build.bat
@@ -1,5 +1,6 @@
 cmake -G "Visual Studio 15 2017 Win64"   ..
 cmake --build . --config Release
-cd Release
-cmake3.exe
+if exist SDL2.dll (echo "find SDL2.dll ") else ( xcopy ..\SDL2-2.0.9\lib\x64\SDL2.dll . )
+xcopy Release\cmake3.exe  . /y
+cmake3.exe airplane.bmp
 pause
