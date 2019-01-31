81ff78ef62d119138d25ec21bcbea3d5aa59fd78
Michael Pan <panyingyun@gmail.com>
2019-01-31 17:12:56 +0800

    add cmakea

---
+++ cmakea/build/auto_build.bat
@@ -0,0 +1,9 @@
+cmake -G "Visual Studio 15 2017 Win64"  ..
+cmake --build . --config Release
+cd Release
+cmakea.exe  16  9
+cmakea.exe  1  25
+cmakea.exe  1  9
+cmakea.exe  2  2
+cmakea.exe  3  3
+pause
---
+++ cmakea/build/auto_build_linux.sh
@@ -0,0 +1,7 @@
+cmake ..
+cmake --build .
+./cmakea  16  9
+./cmakea  1  25
+./cmakea  1  9
+./cmakea  2  2
+./cmakea  3  3
