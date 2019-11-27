56bd565c7415e64cd845cf59d90e2a014cb89f6c
panyingyun <panyingyun@gmail.com>
2019-11-27 23:03:20 +0800

    learn clion ide

--- cmakeb/cmakeb.cpp
+++ cmakeb/cmakeb.cpp
@@ -5,22 +5,22 @@
 #include "msquare/msquare.h"
 
 int main(int argc, char *argv[]) {
-	if (argc < 3) {
-		printf("Usage: input double a and  double b\n");
-		return 1;
-	}
+    if (argc < 3) {
+        printf("Usage: input double a and  double b\n");
+        return 1;
+    }
 
-	double a = atof(argv[1]);
-	double b = atof(argv[2]);
+    double a = atof(argv[1]);
+    double b = atof(argv[2]);
 
 #ifdef USE_SQRT
-	double c = msqrt(a, b);
-	printf("APP Open Macro USE_SQRT\n");
-	printf("sqrt(%f) + sqrt(%f) = %f\n", a, b, c);
+    double c = msqrt(a, b);
+    printf("APP Open Macro USE_SQRT\n");
+    printf("sqrt(%f) + sqrt(%f) = %f\n", a, b, c);
 #else
-	printf("APP Close Macro USE_SQRT\n");
-	double d = msquare(a, b);
-	printf("square(%f) + square(%f) = %f\n", a, b, d);
-#endif //USE_SQRT
-	return 0;
+    printf("APP Close Macro USE_SQRT\n");
+    double d = msquare(a, b);
+    printf("square(%f) + square(%f) = %f\n", a, b, d);
+#endif  // USE_SQRT
+    return 0;
 }
